652d726895df0f6d548e378b0bf0ccbfe0655b8f
GunawanA <adilgunawan222@gmail.com>
2025-12-30 10:50:35 +0700

    create DTO for table Item

---
+++ bth_dc_inventory/DTOs/Item/ItemCreateDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace bth_dc_inventory.DTOs.Item
+{
+    public class ItemCreateDto
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Item Code must be between 2 and 100 characters.")]
+        public string ItemCode { get; set; } = string.Empty;
+
+
+        [Required]
+        [StringLength(200, MinimumLength =3, ErrorMessage ="Item name must be between 3 and 200 characters.")]
+        public string ItemName { get; set; } = string.Empty;
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public int DataCenterId { get; set; } 
+
+        
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Buying price must be a positif number.")]
+        public decimal BuyingPrice { get; set; }
+
+
+    }
+}
---
+++ bth_dc_inventory/DTOs/Item/ItemReadDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace bth_dc_inventory.DTOs.Item
+{
+    public class ItemReadDto
+    {
+        public int Id { get; set; }
+
+        public string ItemCode { get; set; } = string.Empty;
+
+        public string ItemName { get; set; } = string.Empty;
+
+        public string CategoryName { get; set; } = string.Empty; // Nama kategori dari relasi Category
+
+        public string DataCenterName { get; set; } = string.Empty; // Nama data center dari relasi DataCenter
+
+        public decimal BuyingPrice { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Status { get; set; } = string.Empty;
+
+        public DateTime DateOfPurchase { get; set; }
+
+        public DateTime? UpdatedAt { get; set; } = null;
+    }
+}

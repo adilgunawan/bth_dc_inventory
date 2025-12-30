5ce7b5ea31fc211cce8e76e6134ff5c6a3827458
GunawanA <adilgunawan222@gmail.com>
2025-12-30 14:49:26 +0700

    create DTO for table Category

---
+++ bth_dc_inventory/DTOs/Category/CategoryCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bth_dc_inventory.DTOs.Category
+{
+    public class CategoryCreateDto
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; } = string.Empty;
+
+        [StringLength(200)]
+        public string Description { get; set; }  = string.Empty ;
+
+    }
+}
---
+++ bth_dc_inventory/DTOs/Category/CategoryReadDto.cs
@@ -0,0 +1,10 @@
+﻿namespace bth_dc_inventory.DTOs.Category
+{
+    public class CategoryReadDto
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } = string.Empty;    
+        public string Description { get; set; } = string.Empty; 
+        public int TotalItems {  get; set; }
+    }
+}

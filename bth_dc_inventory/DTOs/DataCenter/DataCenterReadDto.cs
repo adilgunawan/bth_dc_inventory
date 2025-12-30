5bdba158248e51de755ac3b20067a0510ef53ed5
GunawanA <adilgunawan222@gmail.com>
2025-12-30 14:47:36 +0700

    create DTO for table DataCenter

---
+++ bth_dc_inventory/DTOs/DataCenter/DataCenterReadDto.cs
@@ -0,0 +1,13 @@
+﻿namespace bth_dc_inventory.DTOs.DataCenter
+{
+    public class DataCenterReadDto
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Location {  get; set; } = string.Empty;
+        public string TotalItems {  get; set; } = string.Empty; 
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}

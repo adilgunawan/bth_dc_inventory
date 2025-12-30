014063147f628285cff9e3bf4f99112a55e5e1a4
GunawanA <adilgunawan222@gmail.com>
2025-12-30 10:50:11 +0700

    create DTO for table user

---
+++ bth_dc_inventory/DTOs/User/UserCreateDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bth_dc_inventory.DTOs.User
+{
+
+    public enum UserRole
+    {
+        User,
+        Admin
+    }
+
+    public class UserCreateDto
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+
+        public string Role { get; set; } = "user";
+    }
+}
---
+++ bth_dc_inventory/DTOs/User/UserUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace bth_dc_inventory.DTOs.User
+{
+    public class UserUpdateDto
+    {
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public string Role { get; set; } = "user";
+    }
+}

61e9548dad95e03828d510100dc5c6588cb3844d
GunawanA <adilgunawan222@gmail.com>
2025-12-31 08:30:25 +0700

    update UserUpdateDto

--- bth_dc_inventory/DTOs/User/UserUpdateDto.cs
+++ bth_dc_inventory/DTOs/User/UserUpdateDto.cs
@@ -1,17 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace bth_dc_inventory.DTOs.User
 {
     public class UserUpdateDto
     {
         [Required]
-        public string UserName { get; set; } = string.Empty;
+        [StringLength(100, MinimumLength = 3)]
+        public string Username { get; set; } = string.Empty;
 
         [Required]
         [EmailAddress]
         public string Email { get; set; } = string.Empty;
-
-        public string Role { get; set; } = "user";
     }
 }

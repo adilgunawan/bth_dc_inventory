fdeb1ab7ea956bffa41cb6e59d3fd55aa1ad5a1e
GunawanA <adilgunawan222@gmail.com>
2026-03-03 16:42:51 +0700

    migrate database

---
+++ bth_dc_inventory/Migrations/20260302024949_RemoveItemCodeUniqueConstraint.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bth_dc_inventory.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveItemCodeUniqueConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

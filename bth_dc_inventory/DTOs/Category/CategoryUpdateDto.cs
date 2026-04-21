cfb7e971c8cdf215d0830620f09dd7d3372c7545
GunawanA <adilgunawan222@gmail.com>
2026-04-21 09:23:56 +0700

    Clean dto Category

--- bth_dc_inventory/DTOs/Category/CategoryUpdateDto.cs
+++ bth_dc_inventory/DTOs/Category/CategoryUpdateDto.cs
@@ -19,25 +19,4 @@ namespace bth_dc_inventory.DTOs.Category
 }
 
 
-//using System.ComponentModel.DataAnnotations;
 
-//namespace bth_dc_inventory.DTOs.Category
-//{
-//    public class CategoryUpdateDto
-//    {
-//        [Required]
-//        public int Id { get; set; }
-
-
-//        [Required]
-//        [StringLength(100, MinimumLength =3)]
-//        public string Name { get; set; } = string.Empty;
-
-//        [StringLength(200)]
-//        public string Description { get; set; } = string.Empty ;
-
-
-
-
-//    }
-//}
